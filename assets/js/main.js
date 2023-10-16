da04c97f864366174c90e2ddc6bfce1c44551c4f
Hamid Mahmood <hamid210545@gmail.com>
2023-10-16 23:44:48 +0500

    uploading personal-portfolio website files

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+const sections = document.querySelectorAll("section[id]");
+
+
+function hello() {
+    console.log("hello");
+}
+
+function navHighliter() {
+    let scrollY = window.pageYOffset;
+    console.log(scrollY)
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute("id");
+
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.sidebar-items a[href*=' + sectionId + ']').classList.add("active-link")
+        } else {
+            document.querySelector('.sidebar-items a[href*=' + sectionId + ']').classList.remove("active-link")
+        }
+    });
+
+}
